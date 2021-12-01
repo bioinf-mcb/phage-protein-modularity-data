5131fa3067e0be54cde6d39267571e8b34188bfb
Krzysztof Szczepaniak <kszczepaniak@Krzysztofs-MacBook-Pro.local>
2021-12-01 18:31:50 +0100

    clutering paths fix

--- lib_phage/clustering.py
+++ lib_phage/clustering.py
@@ -3,7 +3,7 @@
 from subprocess import call
 
 def cluster_proteins(input_fasta_filepath, output_dirpath,
-                     mmseqs_tempdir,
+                     mmseqs_tempdir, mmseqs_binpath,
                      cluster_params_min_seqid = 0.3,
                      cluster_params_sensitivity = 7,
                      cluster_params_coverage = 0.95,
@@ -13,22 +13,23 @@ def cluster_proteins(input_fasta_filepath, output_dirpath,
 
     clustering_filepath = output_dirpath + '/clustering.tsv'
 
-    mmseqs_createdb_cmd = 'mmseqs createdb {} {}/SEQDB'.format(input_fasta_filepath, mmseqs_tempdir)
+    mmseqs_createdb_cmd = '{} createdb {} {}/SEQDB'.format(mmseqs_binpath, input_fasta_filepath, mmseqs_tempdir)
 
 
-    mmseqs_cluster_cmd = 'mmseqs cluster {0}/SEQDB {0}/CLUDB {0} --min-seq-id {1:2f} -s {2:d} -c {3:2f}'.format(
+    mmseqs_cluster_cmd = '{0} cluster {1}/SEQDB {1}/CLUDB {1} --min-seq-id {2:2f} -s {3:d} -c {4:2f}'.format(
+                                  mmseqs_binpath,
                                   mmseqs_tempdir,
                                   cluster_params_min_seqid,
                                   cluster_params_sensitivity,
                                   cluster_params_coverage)
 
 
-    mmseqs_createtsv_cmd = 'mmseqs createtsv {0}/SEQDB {0}/SEQDB {0}/CLUDB {1}'.format(
-                                    mmseqs_tempdir, clustering_filepath)
+    mmseqs_createtsv_cmd = '{0} createtsv {0}/SEQDB {0}/SEQDB {0}/CLUDB {1}'.format(
+                                    mmseqs_binpath, mmseqs_tempdir, clustering_filepath)
 
     ### DEVEL
-    mmseqs_createfasta_cmd = 'mmseqs result2flat {0}/SEQDB {0}/SEQDB {0}/CLUDB {1}'.format(
-                                    mmseqs_tempdir, clustering_filepath.replace('.tsv', '.fasta'))
+    mmseqs_createfasta_cmd = '{0} result2flat {0}/SEQDB {0}/SEQDB {0}/CLUDB {1}'.format(
+                                    mmseqs_binpath, mmseqs_tempdir, clustering_filepath.replace('.tsv', '.fasta'))
 
 
     # print(mmseqs_createdb_cmd)
