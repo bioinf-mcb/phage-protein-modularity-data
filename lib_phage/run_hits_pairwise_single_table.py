3987cb7947eaac6881de9da72e2516a12856f793
Krzysztof Szczepaniak <kszczepaniak@Krzysztofs-MacBook-Pro.local>
2022-08-31 13:13:13 +0200

    rework hits-pairwise to work on smaller dfs

--- lib_phage/run_hits_pairwise_single_table.py
+++ lib_phage/run_hits_pairwise_single_table.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sys
 
 if __name__ == '__main__':
@@ -19,63 +20,66 @@ if __name__ == '__main__':
     # load pair table from path & set output name
     parts_dir = work_dir + 'tmp/prot-families/pair_table_chunks/'
     pair_table = pd.read_csv(parts_dir + 'pair-table-' + str(pair_table_id) + '.csv', sep=',')
-    output_filename = 'repr-hits-pairwise-prob' + str(prob_threshold) + '-' + str(pair_table_id) + '.txt'
 
     # load hhr_table
     table_hhr_filename = all_by_all_output_dir + 'table-hhr.txt'
     table_hhr = pd.read_csv(table_hhr_filename, sep=',')
 
     ## for each pair, calculate prob and cov
-    pair_results = {}
+    pair_table_subsets = np.array_split(pair_table, 40)
 
-    for i, pair in pair_table.iterrows():
-        this_pair_qname = pair['qname']
-        this_pair_sname = pair['sname']
+    for j, pair_sub in enumerate(pair_table_subsets):
+        pair_results = {}
 
-        prob_ab=0
-        prob_ba=0
-        scov_ab=0
-        scov_ba=0
-        qcov_ab=0
-        qcov_ba=0
-        pident_ab=0
-        pident_ba=0
+        for i, pair in pair_sub.iterrows():
+            this_pair_qname = pair['qname']
+            this_pair_sname = pair['sname']
 
-        this_pair_hits_ab = table_hhr[(table_hhr['qname'] == this_pair_qname) & (table_hhr['sname'] == this_pair_sname)]
+            prob_ab=0
+            prob_ba=0
+            scov_ab=0
+            scov_ba=0
+            qcov_ab=0
+            qcov_ba=0
+            pident_ab=0
+            pident_ba=0
 
-        this_pair_prob_cov_ab = get_prob_cov(this_pair_hits_ab, prob_threshold = prob_threshold)
-        if this_pair_prob_cov_ab:
-            prob_ab = this_pair_prob_cov_ab[0]
-            scov_ab = this_pair_prob_cov_ab[1]
-            qcov_ab = this_pair_prob_cov_ab[2]
-            pident_ab = this_pair_prob_cov_ab[3]
+            this_pair_hits_ab = table_hhr[(table_hhr['qname'] == this_pair_qname) & (table_hhr['sname'] == this_pair_sname)]
 
-
-        this_pair_hits_ba = table_hhr[(table_hhr['sname'] == this_pair_qname) & (table_hhr['qname'] == this_pair_sname)]
-
-        this_pair_prob_cov_ba = get_prob_cov(this_pair_hits_ba, prob_threshold = prob_threshold)
-        if this_pair_prob_cov_ba:
-            prob_ba = this_pair_prob_cov_ba[0]
-            scov_ba = this_pair_prob_cov_ba[1]
-            qcov_ba = this_pair_prob_cov_ba[2]
-            pident_ba = this_pair_prob_cov_ba[3]
+            this_pair_prob_cov_ab = get_prob_cov(this_pair_hits_ab, prob_threshold = prob_threshold)
+            if this_pair_prob_cov_ab:
+                prob_ab = this_pair_prob_cov_ab[0]
+                scov_ab = this_pair_prob_cov_ab[1]
+                qcov_ab = this_pair_prob_cov_ab[2]
+                pident_ab = this_pair_prob_cov_ab[3]
 
 
-        min_prob = 0
-        if (prob_ab and prob_ba):
-            min_prob = min(prob_ab, prob_ba)
-        this_pair_prob = min_prob
-        this_pair_scov = min(scov_ab, scov_ba)
-        this_pair_qcov = min(qcov_ab, qcov_ba)
-        min_pident = 0
-        if (pident_ab and pident_ba):
-            min_pident = min(pident_ab, pident_ba)
-        this_pair_pident = min_pident
+            this_pair_hits_ba = table_hhr[(table_hhr['sname'] == this_pair_qname) & (table_hhr['qname'] == this_pair_sname)]
 
-        pair_results[i] = [this_pair_qname, this_pair_sname, this_pair_qcov,
-                           this_pair_scov, this_pair_prob, this_pair_pident]
+            this_pair_prob_cov_ba = get_prob_cov(this_pair_hits_ba, prob_threshold = prob_threshold)
+            if this_pair_prob_cov_ba:
+                prob_ba = this_pair_prob_cov_ba[0]
+                scov_ba = this_pair_prob_cov_ba[1]
+                qcov_ba = this_pair_prob_cov_ba[2]
+                pident_ba = this_pair_prob_cov_ba[3]
 
-    ### now to dataframe and drop to file
-    pair_results_df = pd.DataFrame.from_dict(pair_results, orient='index',
-                                             columns=['qname', 'sname', 'qcov', 'scov', 'prob', 'pident'])
-    pair_results_df.to_csv(families_output_dir + output_filename, index=False, float_format="%.4f")
+
+            min_prob = 0
+            if (prob_ab and prob_ba):
+                min_prob = min(prob_ab, prob_ba)
+            this_pair_prob = min_prob
+            this_pair_scov = min(scov_ab, scov_ba)
+            this_pair_qcov = min(qcov_ab, qcov_ba)
+            min_pident = 0
+            if (pident_ab and pident_ba):
+                min_pident = min(pident_ab, pident_ba)
+            this_pair_pident = min_pident
+
+            pair_results[i] = [this_pair_qname, this_pair_sname, this_pair_qcov,
+                               this_pair_scov, this_pair_prob, this_pair_pident]
+
+        ### now to dataframe and drop to file
+        output_filename = 'repr-hits-pairwise-prob' + str(prob_threshold) + '-' + str(pair_table_id) + '-' + str(j) + '.txt'
+        pair_results_df = pd.DataFrame.from_dict(pair_results, orient='index',
+                                                 columns=['qname', 'sname', 'qcov', 'scov', 'prob', 'pident'])
+        pair_results_df.to_csv(families_output_dir + output_filename, index=False, header=False, float_format="%.4f")
